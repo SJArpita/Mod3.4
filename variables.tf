436e66dc9b42bf8cb81b16b284b2d5490eb197de
SJArpita <rsheea12@gmail.com>
2025-04-14 20:06:58 +0800

    Update

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
-# variable "aws_region" {
-#   default = "us-east-1"
-# }
+variable "aws_region" {
+  default = "us-east-1"
+}
 
-# variable "account_id" {
-#   description = "AWS Account ID"
-#   type        = string
-# }
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
 
-# variable "ecr_repo" {
-#   description = "ECR repository name"
-#   type        = string
-# }
+variable "ecr_repo" {
+  description = "ECR repository name"
+  type        = string
+}
 
-# variable "image_tag" {
-#   description = "ECR image tag (e.g., github.sha)"
-#   type        = string
-# }
+variable "image_tag" {
+  description = "ECR image tag (e.g., github.sha)"
+  type        = string
+}
